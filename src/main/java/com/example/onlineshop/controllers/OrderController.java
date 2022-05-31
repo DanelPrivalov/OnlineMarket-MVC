a50361e280e9d652f2739834bbae5483504773be
IrinaPostnikova <iri.postnikova@gmail.com>
2022-06-01 01:12:20 +0300

    base crud

---
+++ src/main/java/com/example/onlineshop/controllers/OrderController.java
@@ -0,0 +1,58 @@
+package com.example.onlineshop.controllers;
+
+import com.example.onlineshop.entity.order.Order;
+import com.example.onlineshop.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class OrderController {
+    private final OrderRepository orderRepository;
+    @Autowired
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+    @GetMapping("/order")
+    public String findAll(Model model) {
+        List<Order> orders = orderRepository.findAll();
+        model.addAttribute("orders", orders);
+        return "orders";
+    }
+
+    @GetMapping("/order-create")
+    public String createOrderForm(Order order) {
+        return "order-create";
+    }
+
+    @PostMapping("/order-create")
+    public String createOrder(Order order) {
+        orderRepository.save(order);
+        return "redirect:/order";
+    }
+
+    @GetMapping("/order-delete/{orderId}")
+    public String deleteOrder(@PathVariable("orderId") Integer orderId) {
+        orderRepository.deleteById(orderId);
+        return "redirect:/order";
+    }
+
+    @GetMapping("/order-update/{orderId}")
+    public String updateOrderForm(@PathVariable("orderId") Integer orderId, Model model) {
+        Order order = orderRepository.getReferenceById(orderId);
+        model.addAttribute("order", order);
+        return "order-update";
+    }
+
+    @PostMapping("/order-update")
+    public String updateOrder(Order order) {
+
+        orderRepository.save(order);
+        return "redirect:/order";
+    }
+}
--- src/main/java/com/example/onlineshop/entity/order/ProductInOrder.java
+++ src/main/java/com/example/onlineshop/entity/order/ProductInOrder.java
@@ -1,4 +1,29 @@
 package com.example.onlineshop.entity.order;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="product_in_order")
 public class ProductInOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer orderId;
+
+    @Column(name = "product_id")
+    private Integer productId;
+
+    @Column(name = "final_price")
+    private Integer finalPrice;
+
+    @Column(name = "quantity")
+    private Integer quantity;
 }
