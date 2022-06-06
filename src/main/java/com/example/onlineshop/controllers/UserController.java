b1008d57c1f8042750349e44b2c8514a381ae4f3
stacytoffee <stacytoffee@gmail.com>
2022-06-07 00:18:27 +0300

    added user model

--- src/main/java/com/example/onlineshop/controllers/UserController.java
+++ src/main/java/com/example/onlineshop/controllers/UserController.java
@@ -1,4 +1,14 @@
 package com.example.onlineshop.controllers;
 
+import com.example.onlineshop.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
 public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
--- src/main/java/com/example/onlineshop/repository/UserRepository.java
+++ src/main/java/com/example/onlineshop/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.onlineshop.repository;
 
-public interface UserRepository {
+import com.example.onlineshop.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
