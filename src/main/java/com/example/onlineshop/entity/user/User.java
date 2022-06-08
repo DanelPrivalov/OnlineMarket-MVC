5a7e81bff8c225c92bc6411420e3d6e7f41744d0
stacytoffee <stacytoffee@gmail.com>
2022-06-08 23:24:12 +0300

    added first name parameter to User class changes are in UserController and 2 html files

--- src/main/java/com/example/onlineshop/entity/user/User.java
+++ src/main/java/com/example/onlineshop/entity/user/User.java
@@ -12,6 +12,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
