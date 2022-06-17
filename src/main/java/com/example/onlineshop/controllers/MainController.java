b0a8e8b51cdf8f730901cb947ad5ce9b650c3204
PavelErmolin <103310838+PavelErmolin@users.noreply.github.com>
2022-06-18 01:19:31 +0700

    order

--- src/main/java/com/example/onlineshop/controllers/MainController.java
+++ src/main/java/com/example/onlineshop/controllers/MainController.java
@@ -1,8 +1,10 @@
 package com.example.onlineshop.controllers;
 
+import com.example.onlineshop.entity.order.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -10,7 +12,10 @@ public class MainController {
     public String homePage(Model model) {
         return "home";
     }
-
+    @PostMapping("/Basket")
+    public String updateOrder(Order order) {
+        return "redirect:/Basket";
+    }
     @GetMapping("/home")
     public String main(Model model)  {
         model.addAttribute("name","Добро пожаловать");
