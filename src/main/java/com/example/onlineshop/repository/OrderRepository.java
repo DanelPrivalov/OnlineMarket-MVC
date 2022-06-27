e0473c2a5d090f8aa29d6f69cb980d0863259d9b
IrinaPostnikova <iri.postnikova@gmail.com>
2022-06-27 14:56:32 +0300

    мои заказы для пользователя

--- src/main/java/com/example/onlineshop/repository/OrderRepository.java
+++ src/main/java/com/example/onlineshop/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.example.onlineshop.repository;
 
 import com.example.onlineshop.entity.order.Order;
+import com.example.onlineshop.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    public List<Order> findByUser(User user);
 }
